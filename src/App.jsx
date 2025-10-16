9f9c59af87c577f412a31aae35a440973b385f57
Fernanda Genú <nandafmgenu@gmail.com>
2025-10-16 19:45:39 -0300

    fragment

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
   const name = 'Fernanda';
@@ -20,6 +21,7 @@ function App() {
       profissao="Desenvolvedora"
       foto="https://placehold.co/150"
       />
+      <List />
     </div>
   );
 }
